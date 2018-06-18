cee5309ac2b1b10320791fe3e53f30bcd5e380e1
Ulysse94 <32864392+Ulysse94@users.noreply.github.com>
2018-06-19 01:22:24 +0200

    rescponsive V2

--- script/services.js
+++ script/services.js
@@ -1,19 +1,33 @@
-$(document).ready(function() {
-
+$(document).ready(function () {
   var sync1 = $("#sync1");
   var sync2 = $("#sync2");
   var slidesPerPage = 4; //globaly define number of elements per page
   var syncedSecondary = true;
 
+  slides()
+  window.addEventListener("resize",
+    function () {
+      slides()
+    })
+
+  function slides() {
+    if (matchMedia('only screen and (max-width: 750px)').matches) {
+      slidesPerPage = 2;
+    }
+    else {
+      slidesPerPage = 4;
+    }
+  }
+
   sync1.owlCarousel({
-    items : 1,
-    slideSpeed : 2000,
+    items: 1,
+    slideSpeed: 2000,
     nav: true,
     autoplay: false,
     dots: false,
     loop: true,
-    responsiveRefreshRate : 200,
-    navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>','<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
+    responsiveRefreshRate: 200,
+    navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
   }).on('changed.owl.carousel', syncPosition);
 
   sync2
@@ -21,31 +35,31 @@ $(document).ready(function() {
       sync2.find(".owl-item").eq(0).addClass("current");
     })
     .owlCarousel({
-    items : slidesPerPage,
-    dots: false,
-    nav: false,
-    smartSpeed: 200,
-    slideSpeed : 500,
-    slideBy: slidesPerPage, //alternatively you can slide by 1, this way the active slide will stick to the first item in the second carousel
-    responsiveRefreshRate : 100,
-    navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>','<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
-  }).on('changed.owl.carousel', syncPosition2);
+      items: slidesPerPage,
+      dots: false,
+      nav: false,
+      smartSpeed: 200,
+      slideSpeed: 500,
+      slideBy: slidesPerPage, //alternatively you can slide by 1, this way the active slide will stick to the first item in the second carousel
+      responsiveRefreshRate: 100,
+      navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
+    }).on('changed.owl.carousel', syncPosition2);
 
   function syncPosition(el) {
     //if you set loop to false, you have to restore this next line
     //var current = el.item.index;
-    
+
     //if you disable loop you have to comment this block
-    var count = el.item.count-1;
-    var current = Math.round(el.item.index - (el.item.count/2) - .5);
-    
-    if(current < 0) {
+    var count = el.item.count - 1;
+    var current = Math.round(el.item.index - (el.item.count / 2) - .5);
+
+    if (current < 0) {
       current = count;
     }
-    if(current > count) {
+    if (current > count)  {
       current = 0;
     }
-    
+
     //end block
 
     sync2
@@ -56,7 +70,7 @@ $(document).ready(function() {
     var onscreen = sync2.find('.owl-item.active').length - 1;
     var start = sync2.find('.owl-item.active').first().index();
     var end = sync2.find('.owl-item.active').last().index();
-    
+
     if (current > end) {
       sync2.data('owl.carousel').to(current, 100, true);
     }
@@ -64,36 +78,49 @@ $(document).ready(function() {
       sync2.data('owl.carousel').to(current - onscreen, 100, true);
     }
   }
-  
+
   function syncPosition2(el) {
-    if(syncedSecondary) {
+    if (syncedSecondary) {
       var number = el.item.index;
       sync1.data('owl.carousel').to(number, 100, true);
     }
   }
-  
-  sync2.on("click", ".owl-item", function(e){
+
+  sync2.on("click", ".owl-item", function (e) {
     e.preventDefault();
     var number = $(this).index();
     sync1.data('owl.carousel').to(number, 300, true);
   });
 });
 
+var width = $(window).width();
+$(window).resize(function() {
+    if (width > 750 && $(window).width() < 750) {
+        location.reload();
+    }
+    else if (width < 750 && $(window).width() > 750) {
+        location.reload();
+    }
+});
 
-$( "div.info1 li")
-  .mouseenter(function() {
-    $('div.info1 h3').css('margin','40px 0 45px 0');
+$('#sync2 .item').mousemove(function() {
+    $('#sync1 .item').height($('.owl-item.active .item ol').height()+53);
+});
+
+$("div.info1 li")
+  .mouseenter(function () {
+    $('div.info1 h3').css('margin', '40px 0 45px 0');
   });
-$( "div.info1 li" )
-  .mouseleave(function() {
-    $('div.info1 h3').css('margin','90px 0');
+$("div.info1 li")
+  .mouseleave(function () {
+    $('div.info1 h3').css('margin', '90px 0');
   });
 
-  $( "div.info2 li" )
-    .mouseenter(function() {
-      $('div.info2 h3').css('margin','40px 0 45px 0');
-    });
-  $( "div.info2 li" )
-    .mouseleave(function() {
-      $('div.info2 h3').css('margin','90px 0');
-    });
+$("div.info2 li")
+  .mouseenter(function () {
+    $('div.info2 h3').css('margin', '40px 0 45px 0');
+  });
+$("div.info2 li")
+  .mouseleave(function () {
+    $('div.info2 h3').css('margin', '90px 0');
+  });
